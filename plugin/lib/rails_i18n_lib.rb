20c5978640e434ed1252e225545642953de9e674
Stefan Bruhns <sbruhns@users.noreply.github.com>
2016-06-08 21:35:51 +0200

    don't use  named params (compatiblilty issue)

--- plugin/lib/rails_i18n_lib.rb
+++ plugin/lib/rails_i18n_lib.rb
@@ -2,11 +2,11 @@ require "rails_i18n_lib/locale_yaml"
 require "rails_i18n_lib/locale_file_parser"
 
 module RailsI18nLib
-  def self.key_exist?(locale_file_path: ,translation_key:)
+  def self.key_exist?(locale_file_path, translation_key)
     LocaleYaml.new(locale_file_path).has_key?(translation_key)
   end
 
-  def self.get_line_for_key(locale_file_path: ,translation_key:)
+  def self.get_line_for_key(locale_file_path, translation_key)
     LocaleYaml.new(locale_file_path).line_of_key(translation_key)
   end
 end
